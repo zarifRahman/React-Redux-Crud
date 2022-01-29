57be97e13ee38bb13956cc3060feb2737cd4efc6
zarifRahman <zarif.shahriar.rahman@gmail.com>
2022-01-30 01:13:35 +0600

    Add actionTypes

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,12 +1,5 @@
 import React from "react";
 
-// find by id
-// const course = [
-//   { id: 1, title: "Course 1" },
-//   { id: 2, title: "Course 2" },
-// ];
-// course.find((c) => c.id === 2); // output {id: 2, title: "Course 2"}
-
 export function createCourse(course) {
   // debugger;
   return { type: "CREATE_COURSE", course: course };
